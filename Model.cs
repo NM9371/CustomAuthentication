b70902403044052369f66a62bb2665904aa2393f
N371-PC\N371 <n.kosheev@4doc.ru>
2023-11-07 22:30:29 +0300

    .

---
+++ Model.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CustomAuthentication
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=authentication;Username=postgres;Password=321");
+        }
+    }
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
